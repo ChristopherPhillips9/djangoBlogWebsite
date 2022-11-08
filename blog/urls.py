7cbd5c79e64427c8cd1a571f3caaa3c7103424bd
ChristopherPhillips9 <112596258+ChristopherPhillips9@users.noreply.github.com>
2022-11-08 16:04:06 -0500

    Page 105. settings.py has STATIC_URL = "static/" changed to STATIC_URL = "/static/".

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@
 __author__ = "Christopher Phillips, christopher.phillips9@snhu.edu"
 
 from django.urls import path
-from .views import BligListViews
+from .views import BlogListView
 
 urlpatterns = [
     path('', BlogListView.as_view(), name='home')
